bede7fe6adc8f4e0e5bd358ecfedf35fe7266e05
afreensafdar <afreen.leo502@gmail.com>
2021-07-15 15:24:02 -0500

    express route param

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const path = require('path'); //a node native module
-const {Item} = require('./models/index');
-const {Restaurant} = require('./models/index');
+const {Restaurant,Menu,Item} = require('./models/index');
 
 const app = express();
 const port = 3000;
@@ -10,15 +9,27 @@ const port = 3000;
 //Q: What do you think path.join helps us do?  _dirname is the path of directory 
 app.use(express.static(path.join(__dirname, 'public')))
 
+// Add this boilerplate middleware to successfully use req.body
+app.use(express.json())
+
 //will add routes
 // 1)client makes a request -> request URL -> URL -> http request -> http response
 
 //will add routes
+app.get('/', (req, res) => {
+	res.send('hello world')
+})
+
 app.get('/items', async (req, res) => {
 //goes into the database and looks for all Items
     const allItems = await Item.findAll()
     //server will respond with all the items found in the database
     res.json(allItems)
+})
+
+app.get('/menus', async (req, res) => {
+    const allMenus = await Menu.findAll()
+    res.json(allMenus)
 })
 
 app.get('/randomItem', async (req, res) => {
@@ -46,6 +57,81 @@ app.get("/flipcoin", (request, response) => {
     }else{
         response.send("tails");
     }
+})
+
+//Route Params
+//GET restaurants/:id
+/** req.params is an object
+    *this params object contains any URL parameters we state in this route's path (eg id)
+    *Access its values just as any obj property/key
+    *we can then find the restaurants with that id 
+    *and send the user as response.
+    */
+
+app.get('/restaurants/:id', async (req, res) => {
+    let restaurant = await Restaurant.findByPk(req.params.id, {include :Menu});
+	res.json({restaurant})
+})
+
+    //eager loading! We can nest include blocks, to fetch associations of associations
+    const restaurant = await Restaurant.findByPk(req.params.id, {include : {
+        model : Menu,
+        include: Item
+    }});
+    res.json({ restaurant })
+
+
+app.get('/menus/:id', async (req, res) => {
+	let menu = await Menu.findByPk(req.params.id,{include :Item});
+	res.json({menu})
+})
+
+//Get Menu by name, using WHERE
+
+app.get('/menus/name/:title', async (req, res) => {
+    //General querying with association using include
+    const menu = await Menu.findAll({
+        where: {
+            title : req.params.title 
+        }
+    })
+    res.json({ menu })
+})
+
+app.get('/:message/', (req, res) => {
+    res.send(`<h1>req.params.message is ${req.params.message}`);
+})
+
+//whatever we add after : becomes a key on req.params
+app.get('/:message1/:message2', (req, res) => {
+    res.send(`<h1>req.params.message1 is ${req.params.message1} 
+    </h1> <h2>req.params.message2 is ${req.params.message2} </h2>`);
+})
+
+//REST+CRUD
+//testing on postman
+
+// Add new restaurant
+app.post('/restaurants', async (req, res) => {
+	let newRestaurant = await Restaurant.create(req.body);
+	res.send('Restaurant Created Successfully!')
+})
+
+// Delete a restaurant
+
+app.delete('/restaurants/:id', async (req, res) => {
+	await Restaurant.destroy({
+		where : {id : req.params.id} // Destory a restaurant where this object matches
+	})
+	res.send("Restaurant Deleted!!")
+})
+
+// Update a restaurant
+app.put("restaurants/:id", async (req, res) => {
+	let updated = await Restaurant.update(req.body, {
+		where : {id : req.params.id} // Update a restaurant where the id matches, based on req.body
+	})
+	res.send("Restaurant Updated!!")
 })
 
 
